6c5b9a650a9b2551c7f28107c66ed213f4e7f082
cerramaximiliano <cerramaximiliano@gmail.com>
2023-09-26 11:27:02 -0300

    commit validate issues

--- client/src/components/Form/validate.js
+++ client/src/components/Form/validate.js
@@ -5,7 +5,7 @@ export const validate = (data, setValidate) => {
     if( isNaN(data.difficulty) ) errors.difficulty = 'Difficulty must be a number';
     if( Number(data.difficulty) <= 0 || Number(data.difficulty) > 5 ) errors.difficulty = `Difficulty must be a number range 1 to 5`;
     if( isNaN(data.duration) ) errors.duration = `Duration must be a number`
-    if ( data.duration && Number(data.duration) <= 0 && Number(data.duration) > 15 ) errors.duration = `Duration must be a positive number range 1 to 15 `
+    if ( data.duration && (Number(data.duration) <= 0 || Number(data.duration) > 15) ) errors.duration = `Duration must be a positive number range 1 to 15 `
     if( !data.season ) errors.season = `Season ir required`;
     if(!errors.name && !errors.difficulty && !errors.season && !errors.duration) setValidate(true)
     else setValidate(false)
