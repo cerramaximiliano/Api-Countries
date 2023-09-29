de16016c2a281f1723b185b7fe19ef2cfdce1290
cerramaximiliano <cerramaximiliano@gmail.com>
2023-09-29 14:44:17 -0300

    Commit modals

---
+++ client/src/components/Modals/Modal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Modal.module.css"
+
+export default function Modal ()  {
+
+    return (
+        <div className={styles.modalContainer}>
+
+        </div>
+    )
+}
